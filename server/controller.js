a881f82e491b66697da22ae2421adbd256e0b06b
Bryson Elliott <brysonelliott@Brysons-MacBook-Pro.local>
2021-10-07 18:38:05 -0600

    Re-deploy

--- server/controller.js
+++ server/controller.js
@@ -1,21 +1,21 @@
-const about = require('')
-const skills = require('')
-const projects = require('')
-const contact = require('')
+// const about = require('')
+// const skills = require('')
+// const projects = require('')
+// const contact = require('')
 
-module.exports = {
-    getAbout: (req, res) => {
-        res.status(200).send(about)
-    },
-    getSkills: (req, res) => {
-        res.status(200).send(skills)
-    },
-    getProjects: (req, res) => {
-        res.status(200).send(projects)
-    },
-    getContact: (req, res) => {
-        res.status(200).send(contact)
-    },
-}
+// module.exports = {
+//     getAbout: (req, res) => {
+//         res.status(200).send(about)
+//     },
+//     getSkills: (req, res) => {
+//         res.status(200).send(skills)
+//     },
+//     getProjects: (req, res) => {
+//         res.status(200).send(projects)
+//     },
+//     getContact: (req, res) => {
+//         res.status(200).send(contact)
+//     },
+// }
 
 
--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
 const express = require('express')
 const path = require('path')
-const cors = require('cors')
+// const cors = require('cors')
 
 const app = express()
-const ctrl = require('./controller.js')
+// const ctrl = require('./controller.js')
 
 app.use(express.static("client"))
-app.use(cors())
+// app.use(cors())
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'))
 })
 
-app.get('/api/about', ctrl.getAbout)
-app.get('/api/skill', ctrl.getSkills)
-app.get('/api/project', ctrl.getProjects)
-app.get('/api/contact', ctrl.getContact)
+// app.get('/api/about', ctrl.getAbout)
+// app.get('/api/skill', ctrl.getSkills)
+// app.get('/api/project', ctrl.getProjects)
+// app.get('/api/contact', ctrl.getContact)
 
 
 const port = process.env.PORT || 4000
